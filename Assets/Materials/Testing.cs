eb3b5c994bf9561720c87c5c7b97c069c55a9cef
JavierZumer <javier.zumer@gmail.com>
2022-03-27 16:32:25 +0100

    Initial Commit

---
+++ Assets/Materials/Testing.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+// Only want to update if we need to set 3D attributes
+
+/*if (is3D)
+{
+    var transform = GetComponent<Transform>();
+
+    if (GetComponent<Rigidbody>())
+    {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject, rigidBody));
+        RuntimeManager.AttachInstanceToGameObject(instance, transform, rigidBody);
+    }
+    else
+
+    if (GetComponent<Rigidbody2D>())
+    {
+        var rigidBody2D = GetComponent<Rigidbody2D>();
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject, rigidBody2D));
+        RuntimeManager.AttachInstanceToGameObject(instance, transform, rigidBody2D);
+    }
+    else
+
+    {
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+        RuntimeManager.AttachInstanceToGameObject(instance, transform);
+    }*/
+}
---
+++ Assets/Plugins/FMOD/src/StudioListener.cs
@@ -0,0 +1,97 @@
+﻿using FMOD;
+using UnityEngine;
+
+namespace FMODUnity
+{
+    [AddComponentMenu("FMOD Studio/FMOD Studio Listener")]
+    public class StudioListener : MonoBehaviour
+    {
+#if UNITY_PHYSICS_EXIST
+        Rigidbody rigidBody;
+#endif
+#if UNITY_PHYSICS2D_EXIST
+        Rigidbody2D rigidBody2D;
+#endif
+
+        public GameObject attenuationObject;
+
+        public int ListenerNumber = -1;
+        public VelocityVector3 kinematicVelocity = null;
+        Vector3 positionLastFrame;
+
+        void OnEnable()
+        {
+            RuntimeUtils.EnforceLibraryOrder();
+#if UNITY_PHYSICS_EXIST
+            rigidBody = gameObject.GetComponent<Rigidbody>();
+#endif
+#if UNITY_PHYSICS2D_EXIST
+            rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+#endif
+            if (!rigidBody && !rigidBody2D)
+            {
+                kinematicVelocity = new VelocityVector3();
+            }
+            ListenerNumber = RuntimeManager.AddListener(this);
+        }
+
+        void OnDisable()
+        {
+            RuntimeManager.RemoveListener(this);
+        }
+
+        void Update()
+        {
+            if (kinematicVelocity != null)
+            {
+                SetKinematicVelocity();
+            }
+            
+            if (ListenerNumber >= 0 && ListenerNumber < FMOD.CONSTANTS.MAX_LISTENERS)
+            {
+                SetListenerLocation();
+            }
+        }
+        
+        private void SetKinematicVelocity()
+        {
+            //Get current velocity
+            Vector3 currentVel;
+            currentVel.x = kinematicVelocity.x;
+            currentVel.y = kinematicVelocity.y;
+            currentVel.z = kinematicVelocity.z;
+
+            //Update to new velocity
+            currentVel = Vector3.Lerp(currentVel, (transform.position - positionLastFrame) / Time.deltaTime, Time.deltaTime * 15);
+
+            //Reassign to CVector3 object
+            kinematicVelocity.x = currentVel.x;
+            kinematicVelocity.y = currentVel.y;
+            kinematicVelocity.z = currentVel.z;
+
+            //Store world position for next frame
+            positionLastFrame = transform.position;
+        }
+
+        void SetListenerLocation()
+        {
+#if UNITY_PHYSICS_EXIST
+            if (rigidBody)
+            {
+                RuntimeManager.SetListenerLocation(ListenerNumber, gameObject, rigidBody, attenuationObject);
+            }
+            else
+#endif
+#if UNITY_PHYSICS2D_EXIST
+            if (rigidBody2D)
+            {
+                RuntimeManager.SetListenerLocation(ListenerNumber, gameObject, rigidBody2D, attenuationObject);
+            }
+            else
+#endif
+            {
+                RuntimeManager.SetListenerLocation(ListenerNumber, gameObject, kinematicVelocity);
+            }
+        }
+    }
+}
